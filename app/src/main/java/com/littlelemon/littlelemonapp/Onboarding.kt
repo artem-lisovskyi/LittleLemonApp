3028d05ce265925b7e53d93d5a6128eea2127b03
Artem <artemlis2703@gmail.com>
2023-09-01 15:47:33 +0300

    Change Onboarding.kt file

--- app/src/main/java/com/littlelemon/littlelemonapp/Onboarding.kt
+++ app/src/main/java/com/littlelemon/littlelemonapp/Onboarding.kt
@@ -6,21 +6,43 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.littlelemon.littlelemonapp.ui.theme.Green
+import com.littlelemon.littlelemonapp.ui.theme.Yellow
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OnboardingScreen() {
+    var firstName by remember {
+        mutableStateOf("First name")
+    }
+    var secondName by remember {
+        mutableStateOf("Second name")
+    }
+    var email by remember {
+        mutableStateOf("Email")
+    }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
@@ -43,6 +65,78 @@ fun OnboardingScreen() {
                 .background(Green)
                 .wrapContentSize(Alignment.Center)
         )
+        Text(
+            text = "Personal information",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, bottom = 24.dp, top = 48.dp)
+        )
+        Column {
+            Text(
+                text = "First name",
+                fontSize = 16.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+            TextField(
+                value = firstName,
+                onValueChange = { newText -> firstName = newText },
+                textStyle = TextStyle(fontSize = 12.sp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, bottom = 24.dp)
+                    .height(50.dp)
+            )
+
+
+            Text(
+                text = "Second name",
+                fontSize = 16.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+            TextField(
+                value = secondName,
+                onValueChange = { newText -> secondName = newText },
+                textStyle = TextStyle(fontSize = 12.sp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, bottom = 24.dp)
+                    .height(50.dp)
+            )
+            Text(
+                text = "Email",
+                fontSize = 16.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+            TextField(
+                value = email,
+                onValueChange = { newText -> email = newText},
+                textStyle = TextStyle(fontSize = 12.sp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, bottom = 24.dp)
+                    .height(50.dp)
+            )
+
+        }
+        
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(Yellow),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(text = "Register",
+                color = Color.Black)
+        }
     }
 }
 
--- app/src/main/java/com/littlelemon/littlelemonapp/ui/theme/Color.kt
+++ app/src/main/java/com/littlelemon/littlelemonapp/ui/theme/Color.kt
@@ -11,3 +11,4 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val Green = Color(0xFF495E57)
+val Yellow = Color(0xFFF4CE14)
