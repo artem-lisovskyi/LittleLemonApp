f32de9f29b470be54cc83db42c8c35538119e116
Artem <artemlis2703@gmail.com>
2023-09-07 11:17:16 +0300

    Improve category buttons design

--- app/src/main/java/com/littlelemon/littlelemonapp/Home.kt
+++ app/src/main/java/com/littlelemon/littlelemonapp/Home.kt
@@ -3,15 +3,7 @@ package com.littlelemon.littlelemonapp
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -26,13 +18,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -172,7 +158,8 @@ fun HomeScreen(navController: NavHostController, databaseItems: State<List<MenuI
                 ) {
                     Text(
                         text = "All",
-                        color = Color(0xFF495E57)
+                        color = Color(0xFF495E57),
+                        fontSize = 10.sp
                     )
 
                 }
@@ -190,7 +177,8 @@ fun HomeScreen(navController: NavHostController, databaseItems: State<List<MenuI
                 ) {
                     Text(
                         text = "Starters",
-                        color = Color(0xFF495E57)
+                        color = Color(0xFF495E57),
+                        fontSize = 10.sp
                     )
                 }
                 Button(
@@ -208,7 +196,8 @@ fun HomeScreen(navController: NavHostController, databaseItems: State<List<MenuI
                 ) {
                     Text(
                         text = "Mains",
-                        color = Color(0xFF495E57)
+                        color = Color(0xFF495E57),
+                        fontSize = 10.sp
                     )
 
                 }
@@ -227,7 +216,8 @@ fun HomeScreen(navController: NavHostController, databaseItems: State<List<MenuI
                 ) {
                     Text(
                         text = "Desserts",
-                        color = Color(0xFF495E57)
+                        color = Color(0xFF495E57),
+                        fontSize = 10.sp
                     )
 
                 }
